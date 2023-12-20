52edd202da437247c765d7e9c50a0e2ffb11f217
임정찬 <AMAD@MacbookPro.local>
2023-12-20 19:48:35 +0900

    deploy.sh 작성 후 커밋

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+cd /home/ec2-user/app/amad/zip
+sudo mv toyproject-0.0.1-SNAPSHOT.jar /home/ec2-user/docker/project
+cd /home/ec2-user/docker
 
 docker-compose down
 docker-compose up -d --build
